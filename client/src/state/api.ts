8f9b4980568620a283c545a9bcf15481d83610a0
Vesper Yang <43669090+Vesper-Yang@users.noreply.github.com>
2024-10-31 16:31:22 +0800

    前端：配置API提取后端数据

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -2,16 +2,65 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// 这些接口定义是在描述从后端接收的数据结构
+export interface Product {
+  productId: string;
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
+export interface SalesSummary {
+  salesSummaryId: string;
+  totalValue: number;
+  changePercentage?: number;
+  date: string;
+}
+
+export interface PurchaseSummary {
+  purchaseSummaryId: string;
+  totalPurchased: number;
+  changePercentage?: number;
+  date: string;
+}
+
+export interface ExpenseSummary {
+  expenseSummarId: string;
+  totalExpenses: number;
+  date: string;
+}
+
+export interface ExpenseByCategorySummary {
+  expenseByCategorySummaryId: string;
+  category: string;
+  amount: string;
+  date: string;
+}
+
+export interface DashboardMetrics {
+  popularProducts: Product[];
+  salesSummary: SalesSummary[];
+  purchaseSummary: PurchaseSummary[];
+  expenseSummary: ExpenseSummary[];
+  expenseByCategorySummary: ExpenseByCategorySummary[];
+}
+
 export const api = createApi({
   // 设置基础URL，指向后端服务器，对应.env.local http://localhost:8000
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   // 这个名字会在 redux.tsx 中用到，redux持久化存储它的状态
   reducerPath: "api",
   // 用于缓存管理的标签
-  tagTypes: [],
-  // 后续会添加具体的API调用
-  endpoints: (build) => ({}),
+  tagTypes: ["DashboardMetrics"],
+  // 具体的API调用
+  endpoints: (build) => ({
+    getDashboardMetrics: build.query<DashboardMetrics, void>({
+      query: () => "/dashboard",
+      providesTags: ["DashboardMetrics"],
+    }),
+  }),
 });
 
 // 自动生成的hooks
-export const {} = api;
+export const { useGetDashboardMetricsQuery } = api;
